c153861fadbded28dd0f454d609b990e451326c3
Khingc <165073810+Khingc@users.noreply.github.com>
2026-01-28 13:13:10 -0600

    Change trading symbol from IAU to AAPL

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ API_KEY = os.getenv("alpacakey")
 API_SECRET = os.getenv("alpacasecret")
 BASE_URL = "https://paper-api.alpaca.markets"
 
-SYMBOL = "IAU"
+SYMBOL = "AAPL"
 QTY = 1
 SHORT_WINDOW = 5
 LONG_WINDOW = 20
